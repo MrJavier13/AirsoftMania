ad78a5b54cf75cc3886573ef47fc1f52fbd9e4f1
MrJavier13 <128346400+MrJavier13@users.noreply.github.com>
2024-11-14 21:35:09 -0600

    Creación Proyecto
    Creación del proyecto web e integración con JSF y PrimeFaces

---
+++ src/java/Servicio/RedirectController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Servicio;
+
+import java.io.IOException;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import org.primefaces.PrimeFaces;
+
+@ManagedBean(name = "redirectController")
+@SessionScoped
+public class RedirectController implements Serializable{
+
+    
+    public void catalogo() {
+
+        this.redireccionar("/faces/catalogo.xhtml");
+
+    }
+
+    public void redireccionar(String ruta) {
+        HttpServletRequest request;
+        try {
+            request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+            FacesContext.getCurrentInstance().getExternalContext().redirect(request.getContextPath() + ruta);
+        } catch (Exception e) {
+
+        }
+    }
+
+}
